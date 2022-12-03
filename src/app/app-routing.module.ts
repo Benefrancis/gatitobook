f9242006218ecf80ba92df2dfd33b33380aa5514
Benefrancis <benefrancis@gmail.com>
2022-12-03 11:51:44 -0300

    Aula 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path:'', pathMatch:'full', redirectTo: 'home'},
+//lazy Load do meu módulo
+{path:'home', loadChildren:()=>import('./home/home.module').then((m)=>m.HomeModule)}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
